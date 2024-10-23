339389957016d04a575f04c30410d7ffb49e19b8
Youssef <94750744+yusef-777@users.noreply.github.com>
2024-10-23 01:04:27 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,28 @@
-// server.js
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
 const cors = require('cors');
 const matchController = require('./controllers/mainController');
 const disconnectController = require('./controllers/mainController');
-
 const app = express();
 const server = http.createServer(app);
+
+const allowedOrigin = 'https://celebrated-faloodeh-3712a9.netlify.app';
+
 const io = socketIo(server, {
   cors: {
-    origin: 'https://celebrated-faloodeh-3712a9.netlify.app/',
+    origin: allowedOrigin,
     methods: ['GET', 'POST'],
     credentials: true
   }
 });
 
 // Enable CORS
-app.use(cors());
+app.use(cors({ origin: allowedOrigin, credentials: true }));
 
 // Set up socket.io connections
 io.on('connection', (socket) => {
-
   console.log("connected", socket.id);
-
   matchController.handleUserConnection(socket, io);
   disconnectController.handleUserDisconnection(socket, io);
 });
@@ -31,6 +30,3 @@ io.on('connection', (socket) => {
 // Start the server
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-
-
