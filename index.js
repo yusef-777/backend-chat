a4a09b85e171cd22f71b00983b952bdc9acc40ca
yusef-777 <youssefloudini88@gmail.com>
2024-10-28 22:41:27 +0100

    add chat video

--- index.js
+++ index.js
@@ -2,31 +2,24 @@ const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
 const cors = require('cors');
-const matchController = require('./controllers/mainController');
-const disconnectController = require('./controllers/mainController');
+const { handleUserConnection, handleUserDisconnection } = require('./controllers/mainController');
 const app = express();
 const server = http.createServer(app);
-
-const allowedOrigin = 'https://celebrated-faloodeh-3712a9.netlify.app';
-
 const io = socketIo(server, {
   cors: {
-    origin: allowedOrigin,
+    origin: 'http://localhost:3000',
     methods: ['GET', 'POST'],
-    credentials: true
-  }
+    credentials: true,
+  },
 });
 
-// Enable CORS
-app.use(cors({ origin: allowedOrigin, credentials: true }));
+app.use(cors());
 
-// Set up socket.io connections
 io.on('connection', (socket) => {
   console.log("connected", socket.id);
-  matchController.handleUserConnection(socket, io);
-  disconnectController.handleUserDisconnection(socket, io);
+  handleUserConnection(socket, io);
+  handleUserDisconnection(socket, io);
 });
 
-// Start the server
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
